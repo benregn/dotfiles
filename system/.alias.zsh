502db997ff325c10c5df21443e3a464f122d2a3a
Tomas Thor Jonsson <benregn@gmail.com>
2015-03-20 16:12:38 +0100

    replace reload function with reload alias

--- system/.alias.zsh
+++ system/.alias.zsh
@@ -3,3 +3,5 @@ compdef g=git
 
 # https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh
 compdef _git gst=git-status
+
+alias reload='source ~/.zshrc && echo "zsh reloaded."'
