fa228365ce284ea00be1afd26b5e6dee75315f82
Tomas Thor Jonsson <benregn@gmail.com>
2015-04-19 10:11:36 +0200

    remove keybindings for pageup and pagedown - don't use them

--- system/.key-bindings.zsh
+++ system/.key-bindings.zsh
@@ -18,12 +18,6 @@ bindkey -e                                            # Use emacs key bindings
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
 bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
 bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-if [[ "${terminfo[kpp]}" != "" ]]; then
-  bindkey "${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
-fi
-if [[ "${terminfo[knp]}" != "" ]]; then
-  bindkey "${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
-fi
 
 # https://github.com/zsh-users/zsh-history-substring-search
 # Type in any part of any previously entered command and
