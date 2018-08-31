c703587f5a1a70814520337da7260b05c7a5fdf8
Tomas Thor Jonsson <benregn@gmail.com>
2018-08-31 10:46:43 +0200

    Update email-guess with new work email

--- git/git-alias-email-guess.sh
+++ git/git-alias-email-guess.sh
@@ -3,8 +3,8 @@
 remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
 email=benregn@gmail.com # default
 
-if [[ $remote == *github.com:gfish* ]]; then
-  email=tojo@billetto.com
+if [[ $remote == *github.com:meneto* ]]; then
+  email=tojo@meneto.com
 fi
 
 echo "Configuring user.email as $email"
