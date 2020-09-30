a14470474566be23a16abbef4c30c2c70aeada20
Tomas Thor Jonsson <benregn@gmail.com>
2020-09-30 11:23:34 +0200

    Set work email to valified.com

--- symlinks/git/.git-alias-email-guess.sh
+++ symlinks/git/.git-alias-email-guess.sh
@@ -3,8 +3,8 @@
 remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
 email=benregn@gmail.com # default
 
-if [[ $remote == *github.com:Brickshare* ]]; then
-  email=tj@brickshare.io
+if [[ $remote == *github.com:valified* ]]; then
+  email=toj@valified.com
 fi
 
 echo "Configuring user.email as $email"
